4ffbd9b3fcf2c4c6716548a110f15c0b7937afe6
andres.viorels99 <andres.viorels99@gmail.com>
2019-09-28 09:46:23 -0400

    Commit final

---
+++ webApi/webApi/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace webApi.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection") 
+        {
+
+        }
+    }
+}
---
+++ webApi/webApi/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace webApi.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        public int FairyID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24, MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        public Lista Wishes { get; set; }
+        [DataType(DataType.EmailAddress, ErrorMessage = "Direccion de correo no valida")]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+
+
+    }
+    public enum Lista {
+        Amor,
+        Alas,
+        Alma 
+    }
+}
